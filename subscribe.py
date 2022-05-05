05480dd621e07b4e2471924b9f94a0401ff86a05
Pushkar Kadam <pushkarkadam17@outlook.com>
2022-05-05 16:30:37 +1000

    Subscribe code changed to continuously read uplink

--- subscribe.py
+++ subscribe.py
@@ -2,21 +2,31 @@ import os
 import paho.mqtt.subscribe as subscribe
 from dotenv import load_dotenv
 import json
+import time
 
 load_dotenv()
 
 username = os.environ.get("username")
 password = os.environ.get("password")
 
-m = subscribe.simple(topics=['#'],
-                     hostname="au1.cloud.thethings.network",
-                     port=1883,
-                     auth={'username':username,'password':password}, msg_count=2)
+# Runs the programm unless interrupted
+while True:
+    # Waits for an uplink and then proceeds once the message is received
+    m = subscribe.simple(topics=['#'],
+                         hostname="au1.cloud.thethings.network",
+                         port=1883,
+                         auth={'username':username,'password':password})
 
-for a in m:
-    message = a.payload.decode("UTF-8")
+    # Converts the binary to a string
+    message = m.payload.decode("UTF-8")
+
+    # Converts the payload into json object
     payload = json.loads(message)
 
     print('\n\n')
+
+    # extracting device id information
     print(payload["end_device_ids"]["device_id"])
+
+    # Extracting payload data 
     print(payload["uplink_message"]["decoded_payload"])
